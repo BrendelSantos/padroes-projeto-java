63de7ac43b86e9ce72f8722de81e321929de63df
brendel <brendelsantos@gmail.com>
2022-07-24 12:24:26 -0300

    Decorator

---
+++ src/DecoratorPattern/ShapeDecorator.java
@@ -0,0 +1,20 @@
+package DecoratorPattern;
+
+public class ShapeDecorator implements Shape {
+
+    protected Shape decoratedShape;
+
+    public ShapeDecorator(Shape decoratedShape) {
+
+        this.decoratedShape = decoratedShape;
+
+    }
+
+    @Override
+    public void draw() {
+
+        decoratedShape.draw();
+
+    }
+
+}
